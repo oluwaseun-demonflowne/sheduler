26b324a18108a3f1c15445fa0a3e494aff928a50
demonflowne <ibuemmanuel60@gmail.com>
2024-08-21 02:03:13 +0100

    what is happening

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,11 @@ const PORT = 5000;
 app.use("/sendemail", router);
 
 cron.schedule("0 8 * * *", async () => {
-// cron.schedule("* * * * *", async () => {
-//   console.log("function running at 8:00 AM", new Date().toString());
   try {
     // Make a GET request to the /sendemail route
     const response = await axios.get(
       `https://sheduler-qqhc.onrender.com/sendemail`
-    );
+    );;
   } catch (error) {
     console.error("Error calling /sendemail route:", error);
   }
